d6356ce8035940ea621fa1793efb662f3c38a43e
Nick <frankencookie@btinternet.com>
2021-03-11 16:56:25 +0000

    Added engine statics class

--- Assessment/main.cpp
+++ Assessment/main.cpp
@@ -5,27 +5,40 @@
 #include "SDL_ttf.h"
 #include "SDL_mixer.h"
 
+#include "EngineStatics.h"
+
 #undef main
 
 using namespace std;
 
 void main(void) 
 {
+	bool running = true;
+
 	if (SDL_Init(SDL_INIT_EVERYTHING) < 0)
 	{
 		cout << "Could not initialise SDL, SDL Error:" << SDL_GetError() << endl;
+		running = false;
 	}
 	if (IMG_Init(IMG_INIT_PNG) != IMG_INIT_PNG)
 	{
 		cout << "Could not initialise SDL_Image, SDL_Image Error:" << IMG_GetError() << endl;
+		running = false;
 	}
 	if (TTF_Init())
 	{
 		cout << "Could not initialise SDL_TTF, SDL_TTF Error:" << TTF_GetError() << endl;
+		running = false;
 	}
 	if (Mix_Init(MIX_INIT_MP3) != MIX_INIT_MP3)
 	{
 		cout << "Could not initialise SDL_Mixer, SDL_Mixer Error:" << Mix_GetError() << endl;
+		running = false;
+	}
+
+	while (running)
+	{
+		cout << "Update" << endl;
 	}
 
 	SDL_Quit();
