1a73dd3521cab81b243714ef389492eaaba2bef1
JonasLyncke <Jonas@lyncke.de>
2021-04-15 16:19:30 +0100

    Update FontLoader.h

--- Assessment/FontLoader.h
+++ Assessment/FontLoader.h
@@ -1,15 +1,26 @@
 #pragma once
+#include <string.h>
+#include <string>
 
 using namespace std;
 
 class FontLoader 
 {
 public:
-	void loadFontFromFile(String filename);
+	void loadFontFromFile(string filename);
 
 	Font getFontForText(Text text);
 
-	FontLoader();
+	FontLoader(string filename);
 private:
 	std::unordered_map<std::string, const Font*> m_Fonts;
 };
+
+
+
+Constructor(Filepath, Renderer, size, color)
+Update Text(string)
+Change pos(x, y)
+CreateFontTexture()
+GetRect()
+GetTexture()
